46ad06405dd44899c54d8d3787497c051013747e
IT24100370 <it24100370@my.sliit.lk>
2024-08-22 14:22:06 +0530

    Add files via upload

---
+++ Q1/IT24100370Lab4Q1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+	public class IT24100370Lab4Q1
+{
+	public static void main(String[]args)
+	{
+	
+	Scanner input = new Scanner(System.in);
+	double number;
+	
+	System.out.println("Enter a number: ");
+	number = input.nextDouble();
+	
+	if
+		(number > 0)
+
+		System.out.println("The number is: Positive");
+	else if
+		(number < 0)
+		
+		System.out.println("The number is: Negative");
+		 
+	else
+		System.out.println("The number is: zero ");
+		
+	}
+}	
